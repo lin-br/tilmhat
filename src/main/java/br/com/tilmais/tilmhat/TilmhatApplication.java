984bf5bab72907921011ebb9b8b82687d014d5ec
Wesley Ribeiro <wes.rsilva@gmail.com>
2019-07-26 18:00:51 -0300

    Iniciar projeto

---
+++ src/main/java/br/com/tilmais/tilmhat/TilmhatApplication.java
@@ -0,0 +1,13 @@
+package br.com.tilmais.tilmhat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TilmhatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TilmhatApplication.class, args);
+    }
+
+}
