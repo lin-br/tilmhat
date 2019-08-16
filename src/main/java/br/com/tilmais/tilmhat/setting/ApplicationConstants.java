5703a4d1694fc30ed3eef154842db4f2c36e3e72
Wesley Ribeiro <wes.rsilva@gmail.com>
2019-08-16 11:19:09 -0300

    Corrigir path para autenticação

--- src/main/java/br/com/tilmais/tilmhat/setting/ApplicationConstants.java
+++ src/main/java/br/com/tilmais/tilmhat/setting/ApplicationConstants.java
@@ -2,5 +2,5 @@ package br.com.tilmais.tilmhat.setting;
 
 public class ApplicationConstants {
     public static final String PROFILE_TEST = "test";
-    public static final String AUTHENTICATION_PATH = "/api/login";
+    public static final String AUTHENTICATION_PATH = "/login";
 }
